a3d6aa0de59c7906678484d6891beac46b1a7588
Piyush Chaudhary <piyushchry@gmail.com>
2024-04-27 14:40:13 +0530

    Update get_top_anime_for_category_api.dart

--- lib/api/get_top_anime_for_category_api.dart
+++ lib/api/get_top_anime_for_category_api.dart
@@ -15,7 +15,7 @@ Future<String> getCategoryPictureApi({
   final response = await http.get(
     Uri.parse(baseUrl),
     headers: {
-      'X-MAL-CLIENT-ID': "fc55582b725fdf22f30a131b1008aa20",
+      'X-MAL-CLIENT-ID': "Your Client ID",
     },
   );
 
